9d378e022ea86a46cf2f99937a40cfd33a077685
italo renan <italorenan225@gmail.com>
2023-10-11 13:23:52 -0300

    initial commit

---
+++ src/test/java/com/api/freebook/FreebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.freebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
